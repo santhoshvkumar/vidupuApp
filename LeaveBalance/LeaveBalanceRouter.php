2af2bc16799f4244df31132134155162bb3d0d3c
Santhosh Kumar <92086280+rightfit-tech1@users.noreply.github.com>
2025-04-01 12:07:39 +0530

    Removed Apply Leave from Leave Balance

--- LeaveBalance/LeaveBalanceRouter.php
+++ LeaveBalance/LeaveBalanceRouter.php
@@ -1,23 +1,5 @@
 <?php
 
-$f3->route('POST /ApplyLeave',
-    function($f3) {
-        header('Content-Type: application/json');
-        $decoded_items = json_decode($f3->get('BODY'), true);
-        if (!$decoded_items == NULL) {
-            applyLeave($decoded_items);
-        } else {
-            echo json_encode(
-                array(
-                    "status" => "error Leave Balance",
-                    "message_text" => "Invalid input parameters"
-                ),
-                JSON_FORCE_OBJECT
-            );
-        }
-    }
-);
-
 $f3->route('GET /GetLeaveBalance',
     function($f3) {
         header('Content-Type: application/json');
