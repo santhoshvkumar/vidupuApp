63b7220e6469e421140e594c5921f440dfa7ca74
Priyadarshini <priyadarshini.v@tnscbank.com>
2025-04-17 17:22:18 +0530

    changes to dashboard

--- WebApp/Dashboard/DashboardComponent.php
+++ WebApp/Dashboard/DashboardComponent.php
@@ -10,13 +10,35 @@ class DashboardComponent{
         return true;
     }
 
+    public function getActiveEmployees(){
+        include('config.inc');
+        header('Content-Type: application/json');
+        $queryActiveEmployees = "SELECT  count(*) as totalActiveEmployees FROM tblEmployee WHERE isActive = 1";
+        $rsd = mysqli_query($connect_var, $queryActiveEmployees);
+        $row = mysqli_fetch_assoc($rsd);
+        return $row['totalActiveEmployees'];
+    }
+
+    function ActiveEmployees(array $data){
+        $dashboardComponent = new DashboardComponent(); 
+        if($dashboardComponent->loadDashboardDetails($data)){
+            $dashboardComponent->getActiveEmployees();
+        }else{
+            echo json_encode(array("status"=>"error","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+        }
+    }
+
+
+
+
+    
     public function DashboardDetails(){
         include('config.inc');
         header('Content-Type: application/json');
-        try{
             $queryDashboardDetails = "SELECT * FROM tblDashboardDetails";
-            $rsd = mysqli_query($connect_var, $queryDashboardDetails);
-        }
+        $rsd = mysqli_query($connect_var, $queryDashboardDetails);
+        $row = mysqli_fetch_assoc($rsd);
+        return $row;
     }
 
 }
--- WebApp/Dashboard/DashboardRouter.php
+++ WebApp/Dashboard/DashboardRouter.php
@@ -15,6 +15,15 @@ $f3->route('GET /DashboardDetails',
 
 );
 /*****************  End Login User Temp *****************/
-
-
+$f3->route('GET /ActiveEmployees',
+    function($f3){
+        header('Content-Type: application/json');
+        $decoded_items = json_decode($f3->get('BODY'),true);
+        if(!$decoded_items == NULL)
+            ActiveEmployees($decoded_items);
+        else
+            echo json_encode(array("status"=>"error Active Employees","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+    }
+);
+/*****************  End Active Employees *****************/
 ?>
