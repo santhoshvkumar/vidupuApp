ffc60ce73704eda0c2cc0692fbb668a162b75f11
AnandRavi1994 <anandaraman.r@tnscbank.com>
2025-04-22 14:41:01 +0530

    Employees data fetched based on branch completed

--- WebApp/Employee/EmployeeComponent.php
+++ WebApp/Employee/EmployeeComponent.php
@@ -7,12 +7,29 @@ class EmployeeComponent{
     public $PresentBranchID;
     public $NewBranchID;
     public $interChangeDate;
+    public $branchName;
     public function loadEmployeeDetails(array $data){
         if (isset($data['employeeID']) && isset($data['currentBranchID'])) {
             $this->employeeID = $data['employeeID'];
             $this->PresentBranchID = $data['currentBranchID'];
             $this->NewBranchID = $data['newBranchID'];
             $this->interChangeDate = $data['interChangeDate'];
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function loadGetEmployeeDetails(array $data) {
+        if (isset($data['employeeID'])) {
+            $this->employeeID = $data['employeeID'];
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function loadGetEmployeeDetailsBasedOnBranch(array $data) {
+        if (isset($data['branchName'])) {
+            $this->branchName = $data['branchName'];
             return true;
         } else {
             return false;
@@ -79,7 +96,86 @@ class EmployeeComponent{
             ], JSON_FORCE_OBJECT);
         }
     }
-
+    public function GetEmployeeDetails($decoded_items) {
+        include('config.inc');
+        header('Content-Type: application/json');
+    
+        try {
+        
+    
+            // Prepare the SQL query
+            $queryGetEmployeeDetails = "SELECT tblE.empID, tblE.employeeName, tblE.employeePhone,
+            tblE.employeeGender, tblE.Designation, tblB.branchID, tblB.branchName FROM 
+            tblEmployee tblE JOIN tblMapEmp tblM ON tblE.employeeID = tblM.employeeID
+            JOIN tblBranch tblB ON tblM.branchID = tblB.branchID WHERE tblE.employeeID = ?;";
+            
+            $stmt = mysqli_prepare($connect_var, $queryGetEmployeeDetails);
+            mysqli_stmt_bind_param($stmt, "s", $this->employeeID);
+            mysqli_stmt_execute($stmt);
+            
+            // Fetch the result
+            $result = mysqli_stmt_get_result($stmt);
+            $data = mysqli_fetch_assoc($result); // Fetch the associative array
+    
+            // Check if data was fetched
+            if ($data) {
+                echo json_encode([
+                    "status" => "success",
+                    "data" => $data // Return the fetched data
+                ]);
+            } else {
+                echo json_encode([
+                    "status" => "error",
+                    "message_text" => "Unable to get the data of Employee, please check the Employee ID."
+                ], JSON_FORCE_OBJECT);
+            }
+    
+        } catch (Exception $e) {
+            echo json_encode([
+                "status" => "error",
+                "message_text" => $e->getMessage()
+            ], JSON_FORCE_OBJECT);
+        }
+    }
+    public function GetEmployeeBasedOnBranch($decoded_items) {
+        include('config.inc');
+        header('Content-Type: application/json');
+    
+        try {      
+           
+            // Prepare the SQL query
+            $queryGetEmployeeDetailsBasedOnBranch = "SELECT tblE.employeeID, tblE.employeeName, tblE.Designation,
+            tblE.employeePhone FROM tblEmployee tblE JOIN tblmapEmp tblM ON tblE.employeeID = tblM.employeeID
+            JOIN tblBranch tblB ON tblM.branchID = tblB.branchID WHERE tblB.branchName = ?";
+            
+            $stmt = mysqli_prepare($connect_var, $queryGetEmployeeDetailsBasedOnBranch);
+            mysqli_stmt_bind_param($stmt, "s", $this->branchName);
+            mysqli_stmt_execute($stmt);
+            
+            // Fetch the result
+            $result = mysqli_stmt_get_result($stmt);
+            $data = mysqli_fetch_assoc($result); // Fetch the associative array
+    
+            // Check if data was fetched
+            if ($data) {
+                echo json_encode([
+                    "status" => "success",
+                    "data" => $data // Return the fetched data
+                ]);
+            } else {
+                echo json_encode([
+                    "status" => "error",
+                    "message_text" => "Unable to get employee details in selected branch"
+                ], JSON_FORCE_OBJECT);
+            }
+    
+        } catch (Exception $e) {
+            echo json_encode([
+                "status" => "error",
+                "message_text" => $e->getMessage()
+            ], JSON_FORCE_OBJECT);
+        }
+    }
     public function UpdateEmployeeDetails($decoded_items) {
         include('config.inc');
         header('Content-Type: application/json');
@@ -88,7 +184,7 @@ class EmployeeComponent{
             $data = [];    
 
             // 1. Update a branch of Employee due to Transfer to employee
-            $queryUpdateBranchofEmployee = "UPDATE tblmapEmp SET branchID = ? WHERE employeeID = ?;";
+            $queryUpdateBranchofEmployee = "UPDATE tblmapEmp SET branchID = ? WHERE employeeID = ?";
             $stmt = mysqli_prepare($connect_var, $queryUpdateBranchofEmployee);
             mysqli_stmt_bind_param($stmt, "ss", $this->NewBranchID, $this->employeeID);
             mysqli_stmt_execute($stmt);
@@ -138,4 +234,25 @@ function UpdateEmployeeDetails($decoded_items) {
         echo json_encode(array("status" => "error", "message_text" => "Invalid Input Parameters"), JSON_FORCE_OBJECT);
     }
 }
+
+function GetEmployeeDetails($decoded_items) {
+    $EmployeeObject = new EmployeeComponent();
+    if ($EmployeeObject->loadGetEmployeeDetails($decoded_items)) {
+        // Pass the $decoded_items to the UpdateEmployeeDetails method
+        $EmployeeObject->GetEmployeeDetails($decoded_items);
+    } else {
+        echo json_encode(array("status" => "error", "message_text" => "Invalid Input Parameters"), JSON_FORCE_OBJECT);
+    }
+}
+
+function GetEmployeeBasedOnBranch($decoded_items) {
+    $EmployeeObject = new EmployeeComponent();
+    if ($EmployeeObject->loadGetEmployeeDetailsBasedOnBranch($decoded_items)) {
+        // Pass the $decoded_items to the UpdateEmployeeDetails method
+        $EmployeeObject->GetEmployeeBasedOnBranch($decoded_items);
+    } else {
+        echo json_encode(array("status" => "error", "message_text" => "Invalid Input Parameters"), JSON_FORCE_OBJECT);
+    }
+}
+?>
 
--- WebApp/Employee/EmployeeRouter.php
+++ WebApp/Employee/EmployeeRouter.php
@@ -35,5 +35,35 @@ $f3->route('POST /UpdateEmployeeDetails',
     }
 
 );
+$f3->route('POST /GetEmployeeDetails',
+
+    function($f3){
+            header('Content-Type: application/json');
+            $decoded_items = json_decode($f3->get('BODY'),true);
+            if(!$decoded_items == NULL)
+                GetEmployeeDetails($decoded_items);
+            else
+                echo json_encode(array("status"=>"error Login Value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+
+    }
+
+);
+
+$f3->route('POST /GetEmployeeBasedOnBranch',
+
+    function($f3){
+            header('Content-Type: application/json');
+            $decoded_items = json_decode($f3->get('BODY'),true);
+            if(!$decoded_items == NULL)
+                GetEmployeeBasedOnBranch($decoded_items);
+            else
+                echo json_encode(array("status"=>"error Login Value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+
+    }
+
+);
+
+
+
 /*****************  End Login User Temp *****************/
 ?>
