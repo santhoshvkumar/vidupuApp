fa8a129a77406ab07a95b36a8fb2b35951ca32a0
AnandRavi1994 <anandaraman.r@tnscbank.com>
2025-04-29 17:07:57 +0530

    Changes in TranferEmployee Elements

---
+++ WebApp/TransferEmployee/TransferEmployeeComponent.php
@@ -0,0 +1,60 @@
+<?php
+class TransferEmployeeComponent{
+    public $empID;   
+    public $branchName;    
+    public function loadTransferEmployeeDetails(array $data){
+        if (isset($data['empID']) && isset($data['branchID'])) {
+            $this->empID = $data['empID'];
+            $this->branchID = $data['branchID'];            
+            return true;
+        } else {
+            return false;
+        }
+    }     
+
+    public function TransferEmployeeDetails() {
+        include('config.inc');
+        header('Content-Type: application/json');
+    
+        try {
+            $data = [];
+    
+            // 1. Get all active employees Name, ID and BranchID
+            $queryTransferEmployeeDetails = "INSERT INTO tblBranchInterChange (empID, branchID) VALUES (?, ?);";
+            $stmt = mysqli_prepare($connect_var, $queryTransferEmployeeDetails);
+            mysqli_stmt_bind_param($stmt, "ss",
+                $this->empID,
+                $this->branchID
+            );
+
+            if (mysqli_stmt_execute($stmt)) {
+                echo json_encode(array(
+                    "status" => "success",
+                    "message" => "Employee added successfully"
+                ));
+            } else {
+                echo json_encode(array(
+                    "status" => "error",
+                    "message" => "Error adding employee"
+                ));
+            }
+            mysqli_stmt_close($stmt);
+            mysqli_close($connect_var);
+        } catch (Exception $e) {
+            echo json_encode([
+                "status" => "error", 
+                "message_text" => $e->getMessage()
+            ], JSON_FORCE_OBJECT);
+        }
+    }
+
+} // End of 
+function TransferEmployeeDetails($decoded_items) {
+    $EmployeeObject = new TransferEmployeeComponent();
+    if ($EmployeeObject->loadTransferEmployeeDetails($decoded_items)) {
+        $EmployeeObject->TransferEmployeeDetails($decoded_items);
+    } else {
+        echo json_encode(array("status" => "error", "message_text" => "Invalid Input Parameters"), JSON_FORCE_OBJECT);
+    }
+}
+?>
---
+++ WebApp/TransferEmployee/TransferEmployeeRouter.php
@@ -0,0 +1,13 @@
+<?php
+
+/*****************   Transfer Employee Details  *******************/
+$f3->route('POST /TransferEmployeeDetails', function($f3) {
+    $decoded_items = json_decode($f3->get('BODY'), true);
+    if($decoded_items != NULL) {
+        TransferEmployeeDetails($decoded_items);
+    } else {
+        echo json_encode(array("status" => "error", "message_text" => "Invalid Input Parameters"), JSON_FORCE_OBJECT);
+    }
+}); 
+/*****************  End Transfer Employee Details *****************/
+?>
