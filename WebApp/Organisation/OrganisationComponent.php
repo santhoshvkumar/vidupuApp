c539f19def7a19468198564673db35fc7e3f885b
Santhosh Kumar <santhosh@kapiital.com>
2025-06-18 16:38:27 +0530

    Changes for upload image is done

--- WebApp/Organisation/OrganisationComponent.php
+++ WebApp/Organisation/OrganisationComponent.php
@@ -152,7 +152,7 @@ class OrganisationComponent {
                 
                 
                 if (strpos($this->organisationLogo, 'uploads/Organisation/temp/') === 0) {
-                    $baseUploadDir = dirname(__FILE__) . 'uploads/Organisation/';
+                    $baseUploadDir = '/data/server/live/API/public_html/vidupuApp/uploads';
                     $tempFilePath = $baseUploadDir . 'temp/' . basename($this->organisationLogo);
                     $newFolderPath = $baseUploadDir . $latestOrganisationCreatedID . '/';
                     $newFilePath = $newFolderPath . basename($this->organisationLogo);
