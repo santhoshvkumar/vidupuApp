9807d3e66e5395df4f9bcc36f0a1f29dfe048026
Priyadarshini <priyadarshini.v@tnscbank.com>
2025-07-19 16:05:54 +0530

    changes to add leaveRestriction api

--- index.php
+++ index.php
@@ -94,6 +94,9 @@ require 'WebApp/VisitorPass/VisitorPassComponent.php';
 require 'LeaveRestriction/LeaveRestrictionRouter.php';
 require 'LeaveRestriction/LeaveRestrictionComponent.php';
 
+require 'KeyHandling/KeyHandlingRouter.php';
+require 'KeyHandling/KeyHandlingComponent.php';
+
 // Register all routers
 \EmployeePaySlip\EmployeePaySlipRouter::register($f3);
 
