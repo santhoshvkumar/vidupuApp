b551192d66bf388ddddecb9cb5e893f00dce95e5
Santhosh Kumar <santhosh@kapiital.com>
2025-09-06 15:00:32 +0530

    Changes for Attendance Operation for multiple auto checkout is done

--- AttendanceOperation/AttendanceOperationComponent.php
+++ AttendanceOperation/AttendanceOperationComponent.php
@@ -423,7 +423,7 @@ class AttendanceOperationMaster{
             $cutoffTime = '23:59:59'; // End of day cutoff
             $currentDate = $this->dateOfCheckout;
             
-            // Update auto checkout 
+            // Update auto checkout Where Check In Happened and not Checkec Out
             $updateAutoCheckout = "UPDATE tblAttendance
                                 SET 
                                     checkOutTime = '23:59:39',
@@ -583,6 +583,41 @@ class AttendanceOperationMaster{
                 mysqli_stmt_bind_param($attendanceStmt, "s", $employeeID);
                 mysqli_stmt_execute($attendanceStmt);
             }
+
+            // Check for employees with more than 2 auto checkouts in current month
+            $currentMonth = date('Y-m', strtotime($currentDate));
+            $autoCheckoutCountQuery = "SELECT 
+                                        employeeID,
+                                        COUNT(*) as autoCheckoutCount
+                                    FROM tblAttendance 
+                                    WHERE isAutoCheckout = 1 
+                                    AND DATE_FORMAT(attendanceDate, '%Y-%m') = ?
+                                    GROUP BY employeeID
+                                    HAVING COUNT(*) > 2";
+
+            $autoCheckoutCountStmt = mysqli_prepare($connect_var, $autoCheckoutCountQuery);
+            mysqli_stmt_bind_param($autoCheckoutCountStmt, "s", $currentMonth);
+            mysqli_stmt_execute($autoCheckoutCountStmt);
+            $autoCheckoutCountResult = mysqli_stmt_get_result($autoCheckoutCountStmt);
+            
+            // Update isCheckInLocked for employees with more than 2 auto checkouts
+            while ($autoCheckoutRow = mysqli_fetch_assoc($autoCheckoutCountResult)) {
+                $employeeID = $autoCheckoutRow['employeeID'];
+                $autoCheckoutCount = $autoCheckoutRow['autoCheckoutCount'];
+                
+                // Update isCheckInLocked field in tblEmployee
+                $updateCheckInLocked = "UPDATE tblEmployee 
+                                      SET isCheckInLocked = 1 
+                                      WHERE employeeID = ?";
+                
+                $updateLockedStmt = mysqli_prepare($connect_var, $updateCheckInLocked);
+                mysqli_stmt_bind_param($updateLockedStmt, "s", $employeeID);
+                mysqli_stmt_execute($updateLockedStmt);
+                mysqli_stmt_close($updateLockedStmt);
+                
+                error_log("Employee $employeeID locked due to $autoCheckoutCount auto checkouts in $currentMonth");
+            }
+            mysqli_stmt_close($autoCheckoutCountStmt);
 
             // Close all statements
             mysqli_stmt_close($autoCheckoutStmt);
