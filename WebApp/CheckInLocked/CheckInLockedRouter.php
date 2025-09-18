a26b03773124d173ea6adde61ab230ff683cf134
Santhosh Kumar <santhosh@kapiital.com>
2025-09-18 13:36:09 +0530

    Fixed the function close unncessary

--- WebApp/CheckInLocked/CheckInLockedRouter.php
+++ WebApp/CheckInLocked/CheckInLockedRouter.php
@@ -30,7 +30,7 @@ $f3->route('POST /getAllUnLockEmpHistory',
         $decoded_items =  json_decode($f3->get('BODY'),true);
         if(!$decoded_items == NULL)
             getAllUnLockEmpHistory($decoded_items);
-    }
+    
         else
             echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
     }
