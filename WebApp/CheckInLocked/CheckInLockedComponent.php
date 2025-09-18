798694aa8bb835cfe37ef4e8a79218f8ce219e40
Santhosh Kumar <santhosh@kapiital.com>
2025-09-18 13:33:41 +0530

    Changes for CheckIN Locked History is done

--- WebApp/CheckInLocked/CheckInLockedComponent.php
+++ WebApp/CheckInLocked/CheckInLockedComponent.php
@@ -55,6 +55,32 @@ class CheckInLockedComponent{
         }
     }
 
+    public function getAllUnLockEmpHistory() {
+        include('config.inc');
+        $query = "select tblECIL.Reason, tblECIL.CreatedOn, tblU.userName as CreatedBy from tblExcuseCheckInLock tblECIL INNER JOIN tblUser tblU on tblU.userID = tblECIL.CreatedBy where employeeID = ? and DATE_FORMAT(CreatedOn, '%Y-%m') = ?";
+        $stmt = mysqli_prepare($connect_var, $query);
+        mysqli_stmt_bind_param($stmt, "si", $this->employeeID, $this->month);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+    }
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+        if(count($data) > 0) {
+            echo json_encode([
+                "status" => "success",
+                "Count" => count($data),
+                "data" => $data
+            ]);
+        } else {
+            echo json_encode([
+                "status" => "success",
+                "message_text" => "No data found"
+            ], JSON_FORCE_OBJECT);
+        }
+    }
+
     public function getEmployeeLockedHistory() {
         include('config.inc');
         $query = "select tblA.attendanceID, tblA.attendanceDate, tblA.checkInTime, tblB.branchName, tblA.checkOutTime, tblA.TotalWorkingHour, tblA.isLateCheckIN from tblAttendance tblA INNER JOIN tblBranch tblB on tblB.branchID = tblA.checkInBranchID where DATE_FORMAT(attendanceDate, '%Y-%m') = ? and employeeID=? and isAutoCheckout=1";
@@ -106,6 +132,15 @@ function getAllCheckInLockedEmployees($decoded_items) {
     $CheckInLockedObject = new CheckInLockedComponent();
     if ($CheckInLockedObject->loadOrganisationID($decoded_items)) {
         $CheckInLockedObject->getAllCheckInLockedEmployees();
+    } else {
+        echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+    }
+}
+
+function getAllUnLockEmpHistory($decoded_items) {
+    $CheckInLockedObject = new CheckInLockedComponent();
+    if ($CheckInLockedObject->loadEmployeeLockedHistory($decoded_items)) {
+        $CheckInLockedObject->getAllUnLockEmpHistory();
     } else {
         echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
     }
--- WebApp/CheckInLocked/CheckInLockedRouter.php
+++ WebApp/CheckInLocked/CheckInLockedRouter.php
@@ -24,6 +24,18 @@ $f3->route('POST /getAllCheckInLockedEmpHistory',
     }
 );
 
+$f3->route('POST /getAllUnLockEmpHistory',
+    function($f3) {
+        header('Content-Type: application/json');
+        $decoded_items =  json_decode($f3->get('BODY'),true);
+        if(!$decoded_items == NULL)
+            getAllUnLockEmpHistory($decoded_items);
+    }
+        else
+            echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+    }
+);
+
 $f3->route('POST /unlockCheckInLockedEmployee',
     function($f3) {
         header('Content-Type: application/json');
