aeee22a5b76010e48865511cc3fa2e7d105cc16b
Santhosh Kumar <santhosh@kapiital.com>
2025-09-18 11:20:32 +0530

    Changes for Check In is done

--- WebApp/CheckInLocked/CheckInLockedComponent.php
+++ WebApp/CheckInLocked/CheckInLockedComponent.php
@@ -2,10 +2,25 @@
 
 class CheckInLockedComponent{
     public $organisationID;
+    public $employeeID;
+    public $month;
+    
     public function loadOrganisationID(array $data) {
         $this->organisationID = $data['organisationID'];
         return true;
     }
+
+    public function loadEmployeeLockedHistory(array $data) {
+        if(!isset($data['employeeID']) || !isset($data['month'])) {
+            return false;
+        }
+        $this->employeeID = $data['employeeID'];
+        $this->month = $data['month'];
+        return true;
+    }
+
+
+
     public function getAllCheckInLockedEmployees() {
         include('config.inc');
         $query = "SELECT employeeID, empID, employeeName, Designation FROM tblEmployee WHERE isCheckInLocked = 1";
@@ -27,12 +42,47 @@ class CheckInLockedComponent{
             ], JSON_FORCE_OBJECT);
         }
     }
+
+    public function getEmployeeLockedHistory() {
+        include('config.inc');
+        $query = "select * from tblAttendance where DATE_FORMAT(attendanceDate, '%Y-%m') = ? and employeeID=? and isAutoCheckout=1";
+        $stmt = mysqli_prepare($connect_var, $query);
+        mysqli_stmt_bind_param($stmt, "si", $this->month, $this->employeeID);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
+        if(count($data) > 0) {
+            echo json_encode([
+                "status" => "success",
+                "Count" => count($data),
+                "data" => $data
+            ]);
+        } else {
+            echo json_encode([
+                "status" => "error",
+                "message_text" => "No data found"
+            ], JSON_FORCE_OBJECT);
+        }
+    }
 }
 
 function getAllCheckInLockedEmployees($decoded_items) {
     $CheckInLockedObject = new CheckInLockedComponent();
     if ($CheckInLockedObject->loadOrganisationID($decoded_items)) {
         $CheckInLockedObject->getAllCheckInLockedEmployees();
+    } else {
+        echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+    }
+}
+
+function getAllCheckInLockedEmployeeHistory($decoded_items) {
+    $CheckInLockedObject = new CheckInLockedComponent();
+    if ($CheckInLockedObject->loadEmployeeLockedHistory($decoded_items)) {
+        $CheckInLockedObject->getEmployeeLockedHistory();
     } else {
         echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
     }
--- WebApp/CheckInLocked/CheckInLockedRouter.php
+++ WebApp/CheckInLocked/CheckInLockedRouter.php
@@ -3,13 +3,25 @@
 
 $f3->route('POST /getAllCheckInLockedEmployees',
 function($f3) {
-    header('Content-Type: application/json');
-    $decoded_items =  json_decode($f3->get('BODY'),true);
-    if(!$decoded_items == NULL)
-        getAllCheckInLockedEmployees($decoded_items);  
-    else
-        echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
-}
+        header('Content-Type: application/json');
+        $decoded_items =  json_decode($f3->get('BODY'),true);
+        if(!$decoded_items == NULL)
+            getAllCheckInLockedEmployees($decoded_items);  
+        else
+            echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+    }
+);
+
+
+$f3->route('POST /getAllCheckInLockedEmpHistory',
+    function($f3) {
+        header('Content-Type: application/json');
+        $decoded_items =  json_decode($f3->get('BODY'),true);
+        if(!$decoded_items == NULL)
+            getAllCheckInLockedEmployeeHistory($decoded_items);
+        else
+            echo json_encode(array("status"=>"error This value","message_text"=>"Invalid input parameters"),JSON_FORCE_OBJECT);
+    }
 );
 
 ?>
