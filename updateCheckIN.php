fbf286ed2b1b854d98e8d436a6c38767a90a4d4d
Santhosh Kumar <santhosh@kapiital.com>
2025-09-19 17:23:37 +0530

    Changes for Not CHeck in Is added

--- updateCheckIN.php
+++ updateCheckIN.php
@@ -25,7 +25,7 @@
         // Update isCheckInLocked field in tblEmployee
         $updateCheckInLocked = "UPDATE tblEmployee 
                               SET isCheckInLocked = 1 
-                              WHERE employeeID = ? and Designation != 'Deputy General Manager' and Designation != 'Assistant General Manager'";
+                              WHERE employeeID = ? and Designation != 'Deputy General Manager' and Designation != 'Assistant General Manager' and Designation != 'IT Specialist' and Designation !='CISO'";
         
         $updateLockedStmt = mysqli_prepare($connect_var, $updateCheckInLocked);
         mysqli_stmt_bind_param($updateLockedStmt, "s", $employeeID);
