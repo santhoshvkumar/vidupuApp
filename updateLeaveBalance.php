0e42a4bfc89f0fa1052e7e06000361b8e1c72bb1
Santhosh Kumar <santhosh@kapiital.com>
2025-09-19 16:27:20 +0530

    Updated the leave balances

--- updateLeaveBalance.php
+++ updateLeaveBalance.php
@@ -7,22 +7,34 @@
     while($row = mysqli_fetch_assoc($rsd)){
         $employeeID = $row['employeeID'];
         $empID = $row['empID'];
+        $cl_bal = 0;
+        $pl_bal = 0;
+        $ml_bal = 0;
+        $sl_bal = 0;
         echo $employeeID ."----". $empID . "<br>";
         $query = "select cl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'CL'";
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
         $cl_bal = $row['cl_bal'];
         echo "Casual Leave: " . $cl_bal . "<br>";
-        $updateQuery = "update tblLeaveBalance set CasualLeave = '$cl_bal' where employeeID = '$employeeID'";
-        $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
-        echo $empID . " " . $cl_bal . "<br>";
+        if($cl_bal != 0){
+            $updateQuery = "update tblLeaveBalance set CasualLeave = '$cl_bal' where employeeID = '$employeeID'";
+            $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
+            echo $empID . " " . $cl_bal . "<br>";
+        }
         
 
         $query = "select pl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'PL'";
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
         $pl_bal = $row['pl_bal'];
-        $updateQuery = "update tblLeaveBalance set PrivilegeLeave = '$pl_bal' where employeeID = '$employeeID'";
+        echo "Privilege Leave: " . $pl_bal . "<br>";
+        if($pl_bal != 0){
+            $updateQuery = "update tblLeaveBalance set PrivilegeLeave = '$pl_bal' where employeeID = '$employeeID'";
+            $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
+            echo $empID . " " . $pl_bal . "<br>";
+        }
+        
         $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
         echo $empID . " " . $pl_bal . "<br>";
 
@@ -30,18 +42,23 @@
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
         $ml_bal = $row['ml_bal'];
-        $updateQuery = "update tblLeaveBalance set MedicalLeave = '$ml_bal' where employeeID = '$employeeID'";
-        $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
-        echo $empID . " " . $ml_bal . "<br>";
-
+        echo "Medical Leave: " . $ml_bal . "<br>";
+        if($ml_bal != 0){   
+            $updateQuery = "update tblLeaveBalance set MedicalLeave = '$ml_bal' where employeeID = '$employeeID'";
+            $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
+            echo $empID . " " . $ml_bal . "<br>";
+        }
         $query = "select sl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'CO'";
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
         $sl_bal = $row['sl_bal'];
-        $updateQuery = "update tblLeaveBalance set CompensatoryOff = '$sl_bal' where employeeID = '$employeeID'";
-        echo $updateQuery . "<br>";
-        $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
-        echo $empID . " " . $sl_bal . "<br>";
+        echo "Compensatory Off: " . $sl_bal . "<br>";
+        if($sl_bal != 0){
+            $updateQuery = "update tblLeaveBalance set CompensatoryOff = '$sl_bal' where employeeID = '$employeeID'";
+            echo $updateQuery . "<br>";
+            $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
+            echo $empID . " " . $sl_bal . "<br>";
+        }
         
        
 
