cf69a665bb6c3ef307dff591cb8b7efaa4c0a75a
Santhosh Kumar <santhosh@kapiital.com>
2025-09-19 16:31:36 +0530

    Updated the leave balances

--- updateLeaveBalance.php
+++ updateLeaveBalance.php
@@ -24,44 +24,42 @@
         }
         
 
-        $query = "select pl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'PL'";
+        $query = "select cl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'PL'";
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
-        $pl_bal = $row['pl_bal'];
+        $pl_bal = $row['cl_bal'];
         echo "Privilege Leave: " . $pl_bal . "<br>";
         if($pl_bal != 0){
             $updateQuery = "update tblLeaveBalance set PrivilegeLeave = '$pl_bal' where employeeID = '$employeeID'";
             $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
             echo $empID . " " . $pl_bal . "<br>";
         }
-        /*
-        $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
-        echo $empID . " " . $pl_bal . "<br>";
+       
 
-        $query = "select ml_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'SL'";
+        $query = "select cl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'SL'";
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
-        $ml_bal = $row['ml_bal'];
+        $ml_bal = $row['cl_bal'];
         echo "Medical Leave: " . $ml_bal . "<br>";
         if($ml_bal != 0){   
             $updateQuery = "update tblLeaveBalance set MedicalLeave = '$ml_bal' where employeeID = '$employeeID'";
             $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
             echo $empID . " " . $ml_bal . "<br>";
         }
-        $query = "select sl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'CO'";
+        $query = "select cl_bal from leave_data where staff_code = '$empID' and year = 2025 and leave_code = 'CO'";
         $rsdToRunPrivilageCOunt = mysqli_query($connect_var, $query);
         $row = mysqli_fetch_assoc($rsdToRunPrivilageCOunt);
-        $sl_bal = $row['sl_bal'];
+        $sl_bal = $row['cl_bal'];
         echo "Compensatory Off: " . $sl_bal . "<br>";
         if($sl_bal != 0){
             $updateQuery = "update tblLeaveBalance set CompensatoryOff = '$sl_bal' where employeeID = '$employeeID'";
             echo $updateQuery . "<br>";
             $rsdToUpdate = mysqli_query($connect_var, $updateQuery);
             echo $empID . " " . $sl_bal . "<br>";
-        }*/
+        }
         
        
 
     }
-   /* mysqli_close($connect_var);*/
+   mysqli_close($connect_var);
 ?>
